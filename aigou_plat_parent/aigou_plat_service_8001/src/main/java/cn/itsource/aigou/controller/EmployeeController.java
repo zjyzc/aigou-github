b6466f79d0e6777e1fc48e2c72591dd8737b4489
zjyzc <47956733+zjyzc@users.noreply.github.com>
2019-02-25 15:26:04 +0800

    Update EmployeeController.java

--- aigou_plat_parent/aigou_plat_service_8001/src/main/java/cn/itsource/aigou/controller/EmployeeController.java
+++ aigou_plat_parent/aigou_plat_service_8001/src/main/java/cn/itsource/aigou/controller/EmployeeController.java
@@ -17,7 +17,7 @@ public class EmployeeController {
     // @RequestBody 把请求的body中的数据,springmvc的原理封装进去
     @RequestMapping(value = "/login",method = RequestMethod.POST)
     public AjaxResult login(@RequestBody Employee employee) {
-        System.out.println("ddddddddd");
+        System.out.println("dddddd22221111ddd");
         //本来应该获取到用户信息,去和数据库对比:我这里只是模拟:
         if("admin".equals(employee.getName())&&"admin".equals(employee.getPassword())){
             return AjaxResult.me().setSuccess(true).setMsg("登录成功");
